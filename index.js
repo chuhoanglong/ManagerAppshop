018192f0c8a5662f79abe46e6f0a69269f67619e
Hoang Long <hoanglong@Hoangs-MacBook-Pro.local>
2019-10-03 01:25:27 +0700

    commit first

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
-import App from './App';
+import * as serviceWorker from './src/serviceWorker';
+import App from './src/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
